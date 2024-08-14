1f54bc88ce699987c1ab6d59e90f49e634cdd4b7
MacBookPro de Pika <elisehautefaye@gmail.com>
2024-08-14 10:48:46 +0200

    fix utm-to-waze

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/utm-to-waze/", // Assurez-vous que ce chemin correspond au nom de votre dépôt GitHub
+  base: "/utm_to_waze/", // Assurez-vous que ce chemin correspond au nom de votre dépôt GitHub
   plugins: [react()],
 });
