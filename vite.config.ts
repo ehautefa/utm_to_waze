5e34c2f7ed824b6e6ed553db39afdc07a8ae6c1d
MacBookPro de Pika <elisehautefaye@gmail.com>
2025-08-05 15:23:37 +0200

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/utm_to_waze/", // Assurez-vous que ce chemin correspond au nom de votre dépôt GitHub
-  plugins: [react()],
-});
